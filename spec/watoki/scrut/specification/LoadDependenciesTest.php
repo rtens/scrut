77f28f3c23b56b9db37aba77dad5e89ffb70ce99
Nikolas Martens <Nikolas.M@rtens.org>
2013-09-15 01:22:22 +0200

    Refactored tests (most were moved to watoki/factory)

--- spec/watoki/scrut/specification/LoadDependenciesTest.php
+++ spec/watoki/scrut/specification/LoadDependenciesTest.php
@@ -8,14 +8,14 @@ use watoki\scrut\Specification;
  */
 class LoadDependenciesTest extends Specification {
 
-    public function testFullyQualifiedClassNames() {
+    public function testInjectPropertyAnnotations() {
         $this->specification->givenTheClass_InNamespace('SomeFixture', 'spec\watoki\scrut\tmp');
         $this->specification->givenTheClassDefinition('
             /**
              * @property spec\watoki\scrut\tmp\SomeFixture foo<-
              */
             class SomeTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
+                function runAllScenarios($prefix = "test") {
                     $this->setUp();
                 }
             }
@@ -24,146 +24,6 @@ class LoadDependenciesTest extends Specification {
         $this->specification->whenIRunTheTest('SomeTest');
 
         $this->specification->thenItShouldHaveAProperty_WithAnInstanceOf('foo', 'spec\watoki\scrut\tmp\SomeFixture');
-    }
-
-    public function testRelativeNamespace() {
-        $this->specification->givenTheClass_InNamespace('AnotherFixture', 'spec\watoki\scrut\tmp\inside');
-        $this->specification->givenTheClassDefinition('
-            namespace spec\watoki\scrut\tmp;
-
-            /**
-             * @property    inside\AnotherFixture   foo   <-
-             */
-            class RelativeTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
-                    $this->setUp();
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('spec\watoki\scrut\tmp\RelativeTest');
-
-        $this->specification->thenItShouldHaveAProperty_WithAnInstanceOf('foo', 'spec\watoki\scrut\tmp\inside\AnotherFixture');
-    }
-
-    public function testClassAliases() {
-        $this->specification->givenTheClass_InNamespace('AliasedFixture', 'spec\watoki\scrut\tmp');
-        $this->specification->givenTheClassDefinition('
-            use spec\watoki\scrut\tmp\AliasedFixture;
-
-            /**
-             * @property AliasedFixture foo     <-
-             */
-            class AliasTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
-                    $this->setUp();
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('AliasTest');
-
-        $this->specification->thenItShouldHaveAProperty_WithAnInstanceOf('foo', 'spec\watoki\scrut\tmp\AliasedFixture');
-    }
-
-    public function testDontInjectNotMarkedProperties() {
-        $this->specification->givenTheClassDefinition_InFile('
-            class JustSomeFixture extends \watoki\scrut\Fixture {}
-        ', 'JustSomeFixture.php');
-        $this->specification->givenTheClassDefinition('
-            /**
-             * @property JustSomeFixture foo
-             * @property JustSomeFixture bar <-
-             */
-            class NotAllTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
-                    $this->setUp();
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('NotAllTest');
-
-        $this->specification->thenItShouldHaveAProperty_WithAnInstanceOf('bar', 'JustSomeFixture');
-        $this->specification->thenItShouldNoHaveAProperty('foo');
-    }
-
-    public function testInjectPropertyWithDollarSign() {
-        $this->specification->givenTheClassDefinition_InFile('
-            class JustAnotherFixture extends \watoki\scrut\Fixture {}
-        ', 'JustAnotherFixture.php');
-        $this->specification->givenTheClassDefinition('
-            /**
-             * @property JustAnotherFixture $foo <-
-             */
-            class DollarSignTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
-                    $this->setUp();
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('DollarSignTest');
-
-        $this->specification->thenItShouldHaveAProperty_WithAnInstanceOf('foo', 'JustAnotherFixture');
-    }
-
-    public function testInjectProtectedProperty() {
-        $this->specification->givenTheClassDefinition_InFile('
-            class ProtectedFixture extends \watoki\scrut\Fixture {}
-        ', 'ProtectedFixture.php');
-        $this->specification->givenTheClassDefinition('
-            /**
-             * @property ProtectedFixture foo <-
-             */
-            class ProtectedPropertyTest extends \watoki\scrut\Specification {
-                protected $foo;
-                function runAllScenarios() {
-                    $this->setUp();
-                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this->foo);
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('ProtectedPropertyTest');
-
-        $this->then_FixturesShouldBeLoaded(1);
-        $this->thenLoadedFixture_ShouldBe(1, 'ProtectedFixture');
-    }
-
-    public function testOrder() {
-        $this->specification->givenTheClassDefinition_InFile('
-            class FirstFixture extends \watoki\scrut\Fixture {
-                public function __construct(\watoki\scrut\Specification $spec, \watoki\factory\Factory $factory) {
-                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this);
-                }
-            }
-        ', 'FirstFixture.php');
-        $this->specification->givenTheClassDefinition_InFile('
-            class SecondFixture extends \watoki\scrut\Fixture {
-                public function __construct(\watoki\scrut\Specification $spec, \watoki\factory\Factory $factory) {
-                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this);
-                }
-            }
-        ', 'SecondFixture.php');
-
-        $this->specification->givenTheClassDefinition('
-            /**
-             * @property FirstFixture foo1 <-
-             * @property SecondFixture foo2 <-
-             */
-            class OrderTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
-                    $this->setUp();
-                }
-            }
-        ');
-
-        $this->specification->whenIRunTheTest('OrderTest');
-
-        $this->then_FixturesShouldBeLoaded(2);
-        $this->thenLoadedFixture_ShouldBe(1, 'FirstFixture');
-        $this->thenLoadedFixture_ShouldBe(2, 'SecondFixture');
     }
 
     public function testReferenceToTest() {
@@ -180,7 +40,7 @@ class LoadDependenciesTest extends Specification {
              * @property FixtureWithReference foo <-
              */
             class TestReferenceTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
+                function runAllScenarios($prefix = "test") {
                     $this->setUp();
                 }
             }
@@ -197,15 +57,9 @@ class LoadDependenciesTest extends Specification {
         ', 'Dependency.php');
         $this->specification->givenTheClassDefinition_InFile('
             /**
-             * @property Dependency foo <-
+             * @property Dependency bar <-
              */
-            class FixtureWithDependencies extends \watoki\scrut\Fixture {
-                public function __construct(\watoki\scrut\Specification $spec, \watoki\factory\Factory $factory) {
-                    parent::__construct($spec, $factory);
-                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this->foo);
-                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this);
-                }
-            }
+            class FixtureWithDependencies extends \watoki\scrut\Fixture {}
         ', 'FixtureWithDependencies.php');
 
         $this->specification->givenTheClassDefinition('
@@ -213,17 +67,17 @@ class LoadDependenciesTest extends Specification {
              * @property FixtureWithDependencies foo <-
              */
             class FixtureWithDependenciesTest extends \watoki\scrut\Specification {
-                function runAllScenarios() {
+                function runAllScenarios($prefix = "test") {
                     $this->setUp();
+                    spec\watoki\scrut\specification\LoadDependenciesTest::$loaded[] = get_class($this->foo->bar);
                 }
             }
         ');
 
         $this->specification->whenIRunTheTest('FixtureWithDependenciesTest');
 
-        $this->then_FixturesShouldBeLoaded(2);
+        $this->then_FixturesShouldBeLoaded(1);
         $this->thenLoadedFixture_ShouldBe(1, 'Dependency');
-        $this->thenLoadedFixture_ShouldBe(2, 'FixtureWithDependencies');
     }
 
     public static $testReference;
