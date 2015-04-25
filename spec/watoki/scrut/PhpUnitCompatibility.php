625080f0603643eea5a54a6d1792e67a0bccb091
Nikolas Martens <Nikolas.M@rtens.org>
2015-04-25 22:50:04 +0200

    Placeholder specification for PHPUnit compatibility

---
+++ spec/watoki/scrut/PhpUnitCompatibility.php
@@ -0,0 +1,9 @@
+<?php
+namespace watoki\scrut;
+
+/**
+ * In order to facilitate migration, there should be a subclass of StaticTestSuite that
+ * is compatible with PHPUnit_Framework_Assert.
+ */
+class PhpUnitCompatibility {
+}
