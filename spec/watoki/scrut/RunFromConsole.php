4e6c6084a042ccce052f983fe2330b6b89da3c08
Nikolas Martens <Nikolas.M@rtens.org>
2015-04-26 11:40:55 +0200

    Extended Specification compatibility

--- spec/watoki/scrut/RunFromConsole.php
+++ spec/watoki/scrut/RunFromConsole.php
@@ -5,36 +5,36 @@ use watoki\scrut\tests\StaticTestSuite;
 
 class RunFromConsole extends StaticTestSuite {
 
-    function findAutoLoaders() {
-
-    }
-
-    function defaultRunner() {
-        // With default folders
-    }
-
-    function loadConfigFiles() {
-
-    }
-
-    function overwriteDirectory() {
-
-    }
-
-    function configureBootstrap() {
-
-    }
-
-    function configureTestRunner() {
-
-    }
-
-    function configureListeners() {
-
-    }
-
-    function configureTestsSuites() {
-        // directory, plain, static, generic
-        // with different filters (regex, subclass)
-    }
+//    function findAutoLoaders() {
+//
+//    }
+//
+//    function defaultRunner() {
+//        // With default folders
+//    }
+//
+//    function loadConfigFiles() {
+//
+//    }
+//
+//    function overwriteDirectory() {
+//
+//    }
+//
+//    function configureBootstrap() {
+//
+//    }
+//
+//    function configureTestRunner() {
+//
+//    }
+//
+//    function configureListeners() {
+//
+//    }
+//
+//    function configureTestsSuites() {
+//        // directory, plain, static, generic
+//        // with different filters (regex, subclass)
+//    }
 }
--- src/watoki/scrut/tests/migration/Specification.php
+++ src/watoki/scrut/tests/migration/Specification.php
@@ -1,6 +1,25 @@
 <?php
 namespace watoki\scrut\tests\migration;
 
+use watoki\factory\Factory;
+
 class Specification extends PhpUnitTestSuite {
 
-} 
+    /** @var Factory */
+    protected $factory;
+
+    function __construct() {
+        parent::__construct();
+
+        $this->factory = new Factory();
+    }
+
+    protected function before() {
+        parent::before();
+        $this->background();
+    }
+
+    protected function background() {
+    }
+
+}
